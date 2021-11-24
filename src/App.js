49dafda54387cd0c7b9bf020286ea38d6f9d3df8
hasan-sh <hasansh343@gmail.com>
2021-11-24 17:31:02 +0100

    basic gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useCallback } from "react";
+import Carousel, { Modal, ModalGateway } from "react-images";
+
 import './App.css';
+import { photos } from './photos';
+import Gallery from "react-photo-gallery";
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Gallery
+        photos={photos}
+        onClick={openLightbox}
+    />
+    <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
 }
---
+++ src/photos.js
@@ -0,0 +1,23 @@
+export const photos = [
+  {
+    src: "/assets/1.jpg",
+    width: 4,
+    height: 3
+  },
+  
+  {
+    src: "/assets/2.jpg",
+    width: 2,
+    height: 3
+  },
+//   {
+//     src: "/assets/3.jpg",
+//     width: 4,
+//     height: 3
+//   },
+//   {
+//     src: "/assets/2.jpg",
+//     width: 2,
+//     height: 3
+//   },
+]
